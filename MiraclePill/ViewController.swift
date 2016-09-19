c1ee2c72c569073a7ad5120f3aeaa468a32343ce
edrick <edrickepascual@gmail.com>
2016-09-19 16:32:24 -0700

    Add two more textfield and buynow Button

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -9,25 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var statePicker: UIPickerView!
     
     @IBOutlet weak var statePickerButton: UIButton!
     
+    @IBOutlet weak var successImageView: UIImageView!
+    
+    @IBOutlet weak var stateTextfield: UITextField!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    @IBOutlet weak var countryTextfield: UITextField!
+    
+    @IBOutlet weak var zipTextfield: UITextField!
+    
+    @IBOutlet weak var zipLabel: UILabel!
     let states = ["Alaska", "Alabama", "California", "Georgia", "Maine"]
+    
+    
+    func isHidden() {
+        countryLabel.hidden = true
+        countryTextfield.hidden = true
+        zipLabel.hidden = true
+        zipTextfield.hidden = true
+    }
+    
+    func notHidden() {
+        countryLabel.hidden = false
+        countryTextfield.hidden = false
+        zipLabel.hidden = false
+        zipTextfield.hidden = false
+    }
+    
     
     @IBAction func stateButtonPressed(sender: AnyObject) {
         statePicker.hidden = false
+        statePickerButton.hidden = true
+        isHidden()
+    }
+    
+    @IBOutlet weak var buyNowButton: UIButton!
+    
+    @IBAction func buyNowButtonPressed(sender: UIButton) {
         
+        buyNowButton.hidden = true
+        successImageView.hidden = false
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         statePicker.dataSource = self
         statePicker.delegate = self
-        
-        
     }
 
 }
@@ -48,8 +80,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     // Set button to the state selected
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        statePickerButton.setTitle(states[row], forState: UIControlState.Normal)
+//        statePickerButton.setTitle(states[row], forState: UIControlState.Normal)
+        stateTextfield.text = states[row]
         statePicker.hidden = true
+        stateTextfield.hidden = false
+        notHidden()
     }
+    
 }
 
